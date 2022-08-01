12167b4c52fb54d0c3bb19039286bca5ee163a5b
Mahmut Esat <99411717+Elizear@users.noreply.github.com>
2022-08-01 06:16:37 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,5 +1,15 @@
-from wsgiref.simple_server import make_server
+import http.server
+from prometheus_client import start_http_server
+from prometheus_client import Counter
 
-if __name__ == '__main__':
-    httpd = make_server('0.0.0.0', 8080, python)
-    httpd.serve_forever()
+REQUESTS = Counter('server_requests_total', 'Total number of requests to this webserver')
+
+class ServerHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        REQUESTS.inc()
+        self.send_response(200)
+        self.end_headers()
+
+if __name__ == "__main__":
+    server = http.server.HTTPServer(('', 8080), ServerHandler)
+    server.serve_forever()
